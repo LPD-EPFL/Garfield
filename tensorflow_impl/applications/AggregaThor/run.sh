02f1f85c15199fca645a50e1edc92ae5d01c7bf2
Jeremy Plassmann <jeremy.plassmann@gmail.com>
2021-05-03 16:16:17 +0200

    Fix deployement script

--- tensorflow_impl/applications/AggregaThor/run.sh
+++ tensorflow_impl/applications/AggregaThor/run.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
 
-dir=config
 
-if [[ ! -e $dir ]]; then
-    echo "Creating $dir folder" 1>&2
-    mkdir $dir
-else
-    echo "$dir folder already exists, removing previous configuration" 1>&2
-    rm $dir/*
-fi
+common="cd Garfield/tensorflow_impl/applications/AggregaThor/"
+common="$common && python3 trainer.py --log True --max_iter 1000 --batch_size 126"
 
-python3 config_generator.py
+#for filename in config/*; do
+#    echo "$filename"
+#    IP=$(echo $filename | cut -c18- | tr -d '\n')
+#    #echo "ssh -p 9092 root@$IP $common --config $filename < /dev/tty &"
+#    ssh -p 9092 root@$IP "$common --config $filename" < /dev/tty &
+#done
 
-for filename in config/*; do
-    xterm -hold -e python3 trainer.py --config $filename --log True --max_iter 1000 --batch_size 126 & 
-done
+while read p; do
+  ssh ${p%:*} $common --config config/${p} < /dev/tty &
+done < nodes
+
+#ssh -p 9092 root@127.0.0.1 $common --config config/TF_CONFIG-127.0.0.1 < /dev/tty &
+#ssh -p 9093 root@127.0.0.1 $common --config config/TF_CONFIG_127.0.0.1 < /dev/tty &
+
+
+#if [[ ! -e $dir ]]; then
+#    echo "Creating $dir folder" 1>&2
+#    mkdir $dir
+#else
+#     echo "$dir folder already exists, removing previous configuration" 1>&2
+#     rm $dir/*
+# fi
+
+# python3 config_generator.py
+
+# for filename in config/*; do
+#     xterm -hold -e python3 trainer.py --config $filename --log True --max_iter 1000 --batch_size 126 & 
+# done
