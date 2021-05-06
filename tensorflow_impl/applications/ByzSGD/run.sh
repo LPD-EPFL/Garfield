b620816fe88b2cb2cef4a889342e6b1ad32a0395
Jeremy Plassmann <jeremy.plassmann@gmail.com>
2021-05-06 17:29:23 +0200

    deployment script and config file

--- tensorflow_impl/applications/ByzSGD/run.sh
+++ tensorflow_impl/applications/ByzSGD/run.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
-rm config/*
-uniq $OAR_NODEFILE | python3 config_generator.py
+# rm config/*
+# uniq $OAR_NODEFILE | python3 config_generator.py
 
 
-for filename in config/*; do
-    IP=$(echo $filename | cut -c18- | tr -d '\n')
+# for filename in config/*; do
+#     IP=$(echo $filename | cut -c18- | tr -d '\n')
     
-    oarsh $IP python3 Garfield_TF/applications/MSMW/trainer.py \
-        --config Garfield_TF/applications/MSMW/$filename \
-        --log True         \
-        --max_iter 2000    \
-        --batch_size 64    \
-        --dataset cifar10  \
-        --nbbyzwrks 3      \
-        --model VGG &
-done
+#     oarsh $IP python3 Garfield_TF/applications/MSMW/trainer.py \
+#         --config Garfield_TF/applications/MSMW/$filename \
+#         --log True         \
+#         --max_iter 2000    \
+#         --batch_size 64    \
+#         --dataset cifar10  \
+#         --nbbyzwrks 3      \
+#         --model VGG &
+# done
+
+common="cd Garfield/tensorflow_impl/applications/ByzSGD/"
+common="$common && python3 trainer.py  \
+            --log True                 \
+            --max_iter 1000            \
+            --batch_size 64"
+
+
+while read p; do
+  ssh ${p%:*} $common --config config/${p} < /dev/tty &
+done < nodes
--- tensorflow_impl/applications/LEARN/run.sh
+++ tensorflow_impl/applications/LEARN/run.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
-rm -r config/*
-uniq $OAR_NODEFILE | python3 config_generator.py
+# rm -r config/*
+# uniq $OAR_NODEFILE | python3 config_generator.py
 
 
-for filename in config/*; do
-    IP=$(echo $filename | cut -c8- | tr -d '\n')
+# for filename in config/*; do
+#     IP=$(echo $filename | cut -c8- | tr -d '\n')
     
-    oarsh $IP python3 Garfield_TF/applications/LEARN/trainer.py \
-        --config_w Garfield_TF/applications/LEARN/config/$IP/TF_CONFIG_W \
-	    --config_ps Garfield_TF/applications/LEARN/config/$IP/TF_CONFIG_PS \
-       	--log True         \
-        --max_iter 2001     \
-        --batch_size 64    \
-        --dataset cifar10  \
-    	--nbbyzwrks 3      \
-        --model Cifarnet &
-done
+#     oarsh $IP python3 Garfield_TF/applications/LEARN/trainer.py \
+#         --config_w Garfield_TF/applications/LEARN/config/$IP/TF_CONFIG_W \
+# 	    --config_ps Garfield_TF/applications/LEARN/config/$IP/TF_CONFIG_PS \
+#        	--log True         \
+#         --max_iter 2001     \
+#         --batch_size 64    \
+#         --dataset cifar10  \
+#     	--nbbyzwrks 3      \
+#         --model Cifarnet &
+# done
+
+
+
+common="cd Garfield/tensorflow_impl/applications/LEARN/"
+common="$common && python3 trainer.py  \
+            --log True                 \
+            --max_iter 1000            \
+            --batch_size 64"
+
+
+while read p; do
+  ssh ${p%:*} $common --config_w config/${p}/TF_CONFIG_W --config_ps config/${p}/TF_CONFIG_PS < /dev/tty &
+done < nodes
